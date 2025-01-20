82afc68636737a3e354600d717146fda9f24b5d6
bvnderb <brent.vd.bruggen@gmail.com>
2025-01-20 21:39:46 +0100

    updated styling sheets | linked app-header to index.html | made header with an img

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-header',
     standalone: true, // in Angular V19 this is default set to true, in lower versions this is set to false.
-    templateUrl: './header.component.html'
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css' // styleUrls for MULTIPLE css files.
 }) 
 export class HeaderComponent {}
